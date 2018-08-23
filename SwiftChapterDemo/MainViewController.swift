48e02a545ce297b750b05190a847059db974bd88
Cavan_Chio <1403590670@qq.com>
2018-08-23 10:52:34 +0800

    f

---
+++ SwiftChapterDemo/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  SwiftChapterDemo
+//
+//  Created by Cavan on 2018/8/23.
+//  Copyright © 2018年 Cavan_Chio. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationItem.title = "SwiftChapter"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
